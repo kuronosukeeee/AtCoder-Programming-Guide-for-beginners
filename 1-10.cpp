abf945230cd540e474592cfa20332cf375f5a0da
kuronosukeeee <kuronosukeeee@gmail.com>
2023-01-23 21:09:06 +0900

    ex7-10

---
+++ 1-10.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int i = 0;
+  while (i < 5) {
+    cout << i << endl;
+    i++;
+  }
+
+  int N;
+  cin >> N;
+
+  int sum = 0;
+  int x;
+  int i = 0;
+
+  while (i < N)  {
+    cin >> x;
+    sum += x;
+    i++;
+  }
+
+  cout << sum << endl;
+}
--- ex7.cpp
+++ ex7.cpp
@@ -21,7 +21,7 @@ int main() {
   }
   else if (!b || c) {
     cout << "Co";
-  
+  }
 
   if (a && b && c) {
     cout << "foo!";
