5c1c3e8dd09ed657aa38909d944fa43a86fd80c0
kuronosukeeee <kuronosukeeee@gmail.com>
2023-01-29 14:35:07 +0900

    ex.16

---
+++ 2-01.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int keyValue;
+  cin >> keyValue;
+
+  vector<int> vec(5);
+
+  for (int i = 0; i < vec.size(); i++) {
+    cin >> vec.at(i);
+  }
+
+  int cnt = 0;
+
+  for (int i = 0; i < vec.size(); i++) {
+    if (vec.at(i) == keyValue) {
+      cnt++;
+    }
+  }
+
+  cout << cnt << endl;
+
+  for (int x : vec) {
+    cout << x << endl;
+  }
+
+  string str = "hello";
+
+  for (char c : str) {
+    if (c == 'l') {
+      c = 'L';
+    }
+    cout << c;
+  }
+
+  cout << endl;
+
+  int N;
+  cin >> N;
+
+  int cnt2 = 0;
+
+  while (N % 2 == 0 && N > 0) {
+    N = N / 2;
+    cnt2++;
+  }
+
+  cout << cnt2 << endl;
+}
