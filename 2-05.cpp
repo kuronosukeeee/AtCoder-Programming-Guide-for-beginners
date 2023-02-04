756456d26553a7df695af3c31cb68401215d919d
kuronosukeeee <kuronosukeeee@gmail.com>
2023-02-04 17:03:08 +0900

    ex20

--- 2-05.cpp
+++ 2-05.cpp
@@ -97,42 +97,74 @@ using namespace std;
 //   }
 // }
 
-
 // x番の組織について、子組織からの報告書が揃った時刻を返す
 // childrenは組織の関係を表す2次元配列(参照渡し)
-int complete_time(vector<vector<int>> &children, int x) {
-  if (children.at(x).size() == 0) {
-    return 0;
-  }
+// int complete_time(vector<vector<int>> &children, int x) {
+//   if (children.at(x).size() == 0) {
+//     return 0;
+//   }
 
-  int max_receive_time = 0;
+//   int max_receive_time = 0;
 
-  for (int c : children.at(x)) {
-    int receive_time = complete_time(children, c) + 1;
-    max_receive_time = max(max_receive_time, receive_time);
+//   for (int c : children.at(x)) {
+//     int receive_time = complete_time(children, c) + 1;
+//     max_receive_time = max(max_receive_time, receive_time);
+//   }
+//   return max_receive_time;
+// }
+
+// // これ以降の行は変更しなくてよい
+
+// int main() {
+//   int N;
+//   cin >> N;
+
+//   vector<int> p(N);  // 各組織の親組織を示す配列
+//   p.at(0) = -1;  // 0番組織の親組織は存在しないので-1を入れておく
+//   for (int i = 1; i < N; i++) {
+//     cin >> p.at(i);
+//   }
+
+//   // 組織の関係から2次元配列を作る(理解しなくてもよい)
+//   vector<vector<int>> children(N);  // ある組織の子組織の番号一覧  //
+//   N×0の二次元配列 for (int i = 1; i < N; i++) {
+//     int parent = p.at(i);  // i番の親組織の番号
+//     children.at(parent).push_back(i);  // parentの子組織一覧にi番を追加
+//   }
+
+//   // 0番の組織の元に報告書が揃う時刻を求める
+//   cout << complete_time(children, 0) << endl;
+// }
+
+// bool is_even(int);
+// bool is_odd(int);
+
+// bool is_even(int n) {
+//   if (n == 0) {
+//     return true;
+//   }
+//   return is_odd(n - 1);
+// }
+
+// bool is_odd(int n) {
+//   if (n == 0) {
+//     return false;
+//   }
+//   return is_even(n - 1);
+// }
+
+// int main() {
+//   cout << is_even(4) << endl;
+//   cout << is_even(3) << endl;
+//   cout << is_odd(5) << endl;
+//   cout << is_odd(6) << endl;
+// }
+
+string space(int num) {
+  string ret = "";
+  for (int i = 0; i < num; i++) {
+    ret += " ";
+    return ret;
   }
-  return max_receive_time;
 }
 
-// これ以降の行は変更しなくてよい
- 
-int main() {
-  int N;
-  cin >> N;
- 
-  vector<int> p(N);  // 各組織の親組織を示す配列
-  p.at(0) = -1;  // 0番組織の親組織は存在しないので-1を入れておく
-  for (int i = 1; i < N; i++) {
-    cin >> p.at(i);
-  }
- 
-  // 組織の関係から2次元配列を作る(理解しなくてもよい)
-  vector<vector<int>> children(N);  // ある組織の子組織の番号一覧  // N×0の二次元配列
-  for (int i = 1; i < N; i++) {
-    int parent = p.at(i);  // i番の親組織の番号
-    children.at(parent).push_back(i);  // parentの子組織一覧にi番を追加
-  }
- 
-  // 0番の組織の元に報告書が揃う時刻を求める
-  cout << complete_time(children, 0) << endl;
-}
