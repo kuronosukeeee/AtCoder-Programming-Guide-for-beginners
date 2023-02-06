fef755d7ea32f06cf914d7217200fa17037735ab
kuronosukeeee <kuronosukeeee@gmail.com>
2023-02-06 11:41:18 +0900

    ex20

--- 2-05.cpp
+++ 2-05.cpp
@@ -161,40 +161,121 @@ using namespace std;
 // }
  
 // num個分のスペースからなる文字列を返す (字下げに用いる)
-string space(int num) {
-  string ret = "";
-  for (int i = 0; i < num; i++) {
-    ret += " ";
+// string space(int num) {
+//   string ret = "";
+//   for (int i = 0; i < num; i++) {
+//     ret += " ";
+//   }
+//   return ret;
+// }
+ 
+// // 呼び出しの深さに応じて字下げし、関数の開始時点であるというメッセージを出力
+// void print_in(int n, int depth) {
+//   cout << space(depth * 4) << "func(" << n << ", " << depth << ") in" << endl;
+// }
+ 
+// // 呼び出しの深さに応じて字下げし、関数の終了時点であるというメッセージを出力
+// void print_out(int n, int depth) {
+//   cout << space(depth * 4) << "func(" << n << ", " << depth << ") out" << endl;
+// }
+ 
+// // n: 何回の再帰呼出しを行うか
+// // depth: 呼び出しの深さ(何回目の再帰呼び出しか)
+// void func(int n, int depth) {
+//   // ベースケース
+//   if (n == 0) {
+//     print_in(n, depth);   // 開始
+//     print_out(n, depth);  // 終了
+//     return;
+//   }
+ 
+//   // 再帰ステップ
+//   print_in(n, depth);  // 開始
+//   func(n - 1, depth + 1);  // 残り回数を1減らし、呼び出しの深さを1増やす
+//   print_out(n, depth); // 終了
+// }
+ 
+// int main() {
+//   func(3, 0);  // 3回の再帰呼び出しを行う, 初めの深さを0とする
+// }
+
+// 正しい移動かを調べる (y, x)が移動先
+bool is_valid_move(vector<string> &board, vector<vector<bool>> &checked, int x, int y) {
+  int N = board.size();
+ 
+  // 移動先がマス目の外である場合
+  if (x <= -1 || x >= N || y <= -1 || y >= N) {
+    return false;
   }
-  return ret;
+  // 移動先が壁マス
+  if (board.at(y).at(x) == '#') {
+    return false;
+  }
+  // 既に調べているマスへの移動は調べないのでfalseを返す
+  if (checked.at(y).at(x)) {
+    return false;
+  }
+ 
+  // それ以外なら正しい移動
+  return true;
 }
  
-// 呼び出しの深さに応じて字下げし、関数の開始時点であるというメッセージを出力
-void print_in(int n, int depth) {
-  cout << space(depth * 4) << "func(" << n << ", " << depth << ") in" << endl;
-}
+// (y, x)にいる状態からゴールに到達できるか
+// board: マス目の種類
+// checked: そのマスを既に調べたかを持つ二次元配列
+bool reachable(vector<string> &board, vector<vector<bool>> &checked, int x, int y) {
+  int N = board.size();
  
-// 呼び出しの深さに応じて字下げし、関数の終了時点であるというメッセージを出力
-void print_out(int n, int depth) {
-  cout << space(depth * 4) << "func(" << n << ", " << depth << ") out" << endl;
-}
- 
-// n: 何回の再帰呼出しを行うか
-// depth: 呼び出しの深さ(何回目の再帰呼び出しか)
-void func(int n, int depth) {
   // ベースケース
-  if (n == 0) {
-    print_in(n, depth);   // 開始
-    print_out(n, depth);  // 終了
-    return;
+  if (x == N - 1 && y == N - 1) {
+    // ゴールにいる状態
+    return true;
   }
  
   // 再帰ステップ
-  print_in(n, depth);  // 開始
-  func(n - 1, depth + 1);  // 残り回数を1減らし、呼び出しの深さを1増やす
-  print_out(n, depth); // 終了
+ 
+  checked.at(y).at(x) = true;  // 既に調べているという状態に変えておく
+ 
+  // 「上」「右」「下」「左」のいずれかの移動でゴールに到達できるか？
+  bool result = false;
+ 
+  // 上へ移動したマスからゴールに到達できるか？
+  if (is_valid_move(board, checked, x, y - 1) && reachable(board, checked, x, y - 1)) {
+    result = true;
+  }
+  // 右へ移動したマスからゴールに到達できるか？
+  if (is_valid_move(board, checked, x + 1, y) && reachable(board, checked, x + 1, y)) {
+    result = true;
+  }
+  // 下へ移動したマスからゴールに到達できるか？
+  if (is_valid_move(board, checked, x, y + 1) && reachable(board, checked, x, y + 1)) {
+    result = true;
+  }
+  // 左へ移動したマスからゴールに到達できるか？
+  if (is_valid_move(board, checked, x - 1, y) && reachable(board, checked, x - 1, y)) {
+    result = true;
+  }
+ 
+  return result;
 }
  
 int main() {
-  func(3, 0);  // 3回の再帰呼び出しを行う, 初めの深さを0とする
+  int N;
+  cin >> N;
+  // マス目の状態を受け取る
+  vector<string> board(N);
+  for (int i = 0; i < N; i++) {
+    cin >> board.at(i);
+  }
+ 
+  // 既にそのマスを調べたかを保持する二次元配列
+  vector<vector<bool>> checked(N, vector<bool>(N, false));  // false(まだ調べていない)で初期化しておく
+ 
+  // (0, 0) からゴールまで到達できるか？
+  if (reachable(board, checked, 0, 0)) {
+    cout << "Yes" << endl;
+  }
+    else {
+    cout << "No" << endl;
+  }
 }
